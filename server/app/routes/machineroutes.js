dd27424575ec8fea99fed0a4f3312dceced481e5
Lagna <richy_lion@yahoo.com>
2019-04-20 22:56:27 +0700

    add some route

---
+++ server/app/routes/machineroutes.js
@@ -0,0 +1,18 @@
+'use strict';
+const express = require('express')
+const machineController = require('../controllers/machineController');
+
+const router = express.Router()
+
+
+router.route('/')
+  .get(machineController.list_all_machines)
+  .post(machineController.create_machine);
+   
+router.route('/:MachineID')
+  .get(machineController.read_a_machine)
+  .put(machineController.update_a_machine)
+  .delete(machineController.delete_a_machine);
+
+
+module.exports = router
