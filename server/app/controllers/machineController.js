3397604af9514569fb11c4fbeb605bdb8351811a
Lagna <richy_lion@yahoo.com>
2019-05-09 19:41:45 +0700

    fix ip address

--- server/app/controllers/machineController.js
+++ server/app/controllers/machineController.js
@@ -67,24 +67,18 @@ exports.update_status = function(id, status){
   Machine.updatestatus(id, status)
 }
 
+var getClientAddress = function (req) {
+  return (req.headers['x-forwarded-for'] || '').split(',')[0] 
+      || req.connection.remoteAddress;
+};
+
 exports.update_ip = function(req, res) {
-  var ipAddress;
-  // The request may be forwarded from local web server.
-  var forwardedIpsStr = req.header('x-forwarded-for'); 
-  if (forwardedIpsStr) {
-    // 'x-forwarded-for' header may return multiple IP addresses in
-    // the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
-    // the first one
-    var forwardedIps = forwardedIpsStr.split(',');
-    ipAddress = forwardedIps[0];
-  }
-  if (!ipAddress) {
-    // If request was not forwarded
-    ipAddress = req.connection.remoteAddress;
-  }
+  var ipAddress = getClientAddress(req);
+  
   Machine.updateip(req.params.MachineID, ipAddress, function(err, machine) {
     if (err)
       res.send(err);
     res.json(machine);
   })
 }
+
